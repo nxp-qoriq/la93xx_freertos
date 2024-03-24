ae4caffe7d9c716227c641e72c6522a2d25b3ae6
Davide Cavion <davide@rfnm.io>
2024-03-24 15:37:41 +0100

    Add rfnm_rf_ctrl TDD struct

---
+++ Demo/CORTEX_M4_NXP_LA9310_GCC/include/rfnm_rf_ctrl.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+/*
+ * Copyright 2024 NXP
+ */
+
+#ifndef __RFNM_RF_CTRL_H__
+#define __RFNM_RF_CTRL_H__
+
+
+#define PHYTIMER_500_US_61p44 	30720
+
+/* RF control structure */
+typedef struct {
+	uint32_t target_phytimer_ts;
+	uint32_t issued_phytimer_ts;
+	// GPT3 is used as TTI trigger inside the i.MX. To enable it, set 
+	// tti_period_ts > 0. TTI will then trigger at the same time as
+	// target_phytimer_ts. tti_period_ts > 0 should only be sent once,
+	// unless you need to change the period, send tti_period_ts = 0
+	// to disable the interrupt, set tti_period_ts = 1
+	uint32_t tti_period_ts;
+	uint32_t mode;
+} rf_ctrl_s;
+
+void switch_rf(uint32_t mode);
+
+#endif
